17c65294d5a8a5d6e766699048169b51e6095961
Angel Cruz <angelcruzcespedes797@gmail.com>
2026-02-07 09:05:02 -0400

    Correccion de texto

--- src/pages/home/components/HomeScreen.tsx
+++ src/pages/home/components/HomeScreen.tsx
@@ -2,7 +2,7 @@ export default function HomeScreen() {
   return (
     <>
       <div className="px-20 pt-10">
-        <h1 className="text-4xl mb-10">Bievenido al convertidor de NovaSis</h1>
+        <h1 className="text-4xl mb-10">Bienvenido al convertidor de NovaSis</h1>
         <span className="text-gray-600 mb-2">Esta diseñado para que puedas converir los numeros que desea convetir.</span>
         <span className="text-gray-600"> Usando menu de arriba te puedes dirigir al tipo de convertidor que desees</span>
       </div>
